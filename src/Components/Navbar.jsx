cad9d9fc4aedf072c6284549beeedeb738581d32
Prathammehra7 <manshumehra7@gmail.com>
2022-12-23 16:24:39 +0530

    final update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { MdOutlineClose } from 'react-icons/md'
 import Logo from '../Image/logo.png'
 // import { links } from '../data'
--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import Header from '../../Components/Header'
-// import HeaderImage from '../../Images/header_bg_1.jpg'
-// import StoryImage from '../../Images/about1.jpg'
+// import Header from '../../Components/Header'
 import about2 from '../../Image/about2.jpg'
 import about1 from '../../Image/about1.jpg'
 import './About.css'
@@ -10,31 +8,25 @@ import './About.css'
 const About = () => {
   return (
     <>
-    
-
       <section className="about__story" >
         <div className="container about__story-container">
-          {/* <div className="about__section-image">
-            <img src={StoryImage} alt="Our Story Image" />
-          </div> */}
-
           <div className="about__section-content">
             <h1>Our Story</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo modi, inventore nam 
-              quasi odit praesentium, cumque sequi aspernatur officiis nisi repellat nesciunt aliquam 
-               sed consequatur facilis, harum labore consectetur!
-               </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo modi, inventore nam
+              quasi odit praesentium, cumque sequi aspernatur officiis nisi repellat nesciunt aliquam
+              sed consequatur facilis, harum labore consectetur!
+            </p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse labore commodi at animi
-               exercitationem ut minus officia hic pariatur quo illo sit ad possimus, consequatur beatae
-                ullam assumenda dolorum ipsum quaerat quasi, minima sed iure soluta vel? Voluptates,
-                voluptatum earum?
-                </p>
-            
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo modi, inventore nam 
-              quasi odit praesentium, cumque sequi aspernatur officiis nisi repellat nesciunt aliquam 
-               sed consequatur facilis, harum labore exercitationem ut minus officia hic pariatur quo illo sit ad possimus, consequatur beatae
-                ullam assumenda consectetur!
-               </p>
+              exercitationem ut minus officia hic pariatur quo illo sit ad possimus, consequatur beatae
+              ullam assumenda dolorum ipsum quaerat quasi, minima sed iure soluta vel? Voluptates,
+              voluptatum earum?
+            </p>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo modi, inventore nam
+              quasi odit praesentium, cumque sequi aspernatur officiis nisi repellat nesciunt aliquam
+              sed consequatur facilis, harum labore exercitationem ut minus officia hic pariatur quo illo sit ad possimus, consequatur beatae
+              ullam assumenda consectetur!
+            </p>
           </div>
 
         </div>
@@ -44,71 +36,67 @@ const About = () => {
         <div className="container about__vision-container">
           <div className="about__section-content">
             <h1>Our Vision</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo modi, inventore nam 
-              quasi odit praesentium, cumque sequi aspernatur officiis nisi repellat nesciunt aliquam 
-               sed consequatur facilis, harum labore consectetur!
-               </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo modi, inventore nam
+              quasi odit praesentium, cumque sequi aspernatur officiis nisi repellat nesciunt aliquam
+              sed consequatur facilis, harum labore consectetur!
+            </p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse labore commodi at animi
-               exercitationem ut minus officia hic pariatur quo illo sit ad possimus, consequatur beatae
-                ullam assumenda dolorum ipsum quaerat quasi, minima sed iure soluta vel? Voluptates,
-                voluptatum earum?
-                </p>
-            
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo modi, inventore nam 
-              quasi odit praesentium, cumque sequi aspernatur officiis nisi repellat nesciunt aliquam 
-               sed consequatur facilis, harum labore exercitationem ut minus officia hic pariatur quo illo sit ad possimus, consequatur beatae
-                ullam assumenda consectetur!
-               </p>
+              exercitationem ut minus officia hic pariatur quo illo sit ad possimus, consequatur beatae
+              ullam assumenda dolorum ipsum quaerat quasi, minima sed iure soluta vel? Voluptates,
+              voluptatum earum?
+            </p>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo modi, inventore nam
+              quasi odit praesentium, cumque sequi aspernatur officiis nisi repellat nesciunt aliquam
+              sed consequatur facilis, harum labore exercitationem ut minus officia hic pariatur quo illo sit ad possimus, consequatur beatae
+              ullam assumenda consectetur!
+            </p>
           </div>
-          {/* <div className="about__section-image">
-            <img src={VisionImage} alt="Our vision Image" />
-          </div> */}
+
         </div>
       </section>
 
 
 
-      
+
       <section className="about__mission">
         <div className="container about__mission-container">
-          {/* <div className="about__section-image">
-            <img src={MissionImage} alt="Our mission Image" />
-          </div> */}
+
 
           <div className="about__section-content">
             <h1>Our Values</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo modi, inventore nam 
-              quasi odit praesentium, cumque sequi aspernatur officiis nisi repellat nesciunt aliquam 
-               sed consequatur facilis, harum labore consectetur!
-               </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo modi, inventore nam
+              quasi odit praesentium, cumque sequi aspernatur officiis nisi repellat nesciunt aliquam
+              sed consequatur facilis, harum labore consectetur!
+            </p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse labore commodi at animi
-               exercitationem ut minus officia hic pariatur quo illo sit ad possimus, consequatur beatae
-                ullam assumenda dolorum ipsum quaerat quasi, minima sed iure soluta vel? Voluptates,
-                voluptatum earum?
-                </p>
-            
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo modi, inventore nam 
-              quasi odit praesentium, cumque sequi aspernatur officiis nisi repellat nesciunt aliquam 
-               sed consequatur facilis, harum labore exercitationem ut minus officia hic pariatur quo illo sit ad possimus, consequatur beatae
-                ullam assumenda consectetur!
-               </p>
+              exercitationem ut minus officia hic pariatur quo illo sit ad possimus, consequatur beatae
+              ullam assumenda dolorum ipsum quaerat quasi, minima sed iure soluta vel? Voluptates,
+              voluptatum earum?
+            </p>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo modi, inventore nam
+              quasi odit praesentium, cumque sequi aspernatur officiis nisi repellat nesciunt aliquam
+              sed consequatur facilis, harum labore exercitationem ut minus officia hic pariatur quo illo sit ad possimus, consequatur beatae
+              ullam assumenda consectetur!
+            </p>
           </div>
         </div>
-           <div className=" container about__section-image">
-            <img src={about1} alt="Our mission Image" />
-            <img src={about2} alt="Our mission Image" />
-          </div>
-           <div className=" container about__item-image">
-            <h4>Customer Service</h4>
-            <h4>Communication </h4>
-           
-          </div>
-           <div className=" container about__text-image">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, consequatur?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, perspiciatis?</p>
-          </div>
+        <div className=" container about__section-image">
+          <img src={about1} alt="" />
+          <img src={about2} alt="" />
+        </div>
+        <div className=" container about__item-image">
+          <h4>Customer Service</h4>
+          <h4>Communication </h4>
+
+        </div>
+        <div className=" container about__text-image">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, consequatur?</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, perspiciatis?</p>
+        </div>
       </section>
-      
+
 
 
     </>
